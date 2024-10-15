7e0c69ab2c80296cac381194769dba0bb4c71be3
Amaimono <k023g0048@g.neec.ac.jp>
2024-10-15 12:23:59 +0900

    Thread class

---
+++ code/engine/Thread/Thread.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include <concepts>
+#include <functional>
+#include <vector>
+
+#include <mutex>
+#include <queue>
+#include <thread>
+
+#include <stdint.h>
+
+class IThread
+{
+public:
+	IThread() = default;
+	virtual ~IThread(){}
+
+	virtual void Init(int32_t threadNum);
+	virtual void Finalize();
+protected:
+	virtual void LoopUpdate() = 0;
+protected:
+	std::vector<std::thread> threads_;
+	std::mutex  mutex_;
+	std::condition_variable condition_;
+	bool stopThread_;
+public:
+	void StopThread() { 
+
+		std::lock_guard<std::mutex> lock(mutex_);
+		stopThread_ = true; 
+	}
+	void RunThread()  { 
+		std::lock_guard<std::mutex> lock(mutex_);
+		stopThread_ = false; 
+	}
+};
+
+template<typename T>
+concept HaveUpdate = requires(T t)
+{
+	{ t.Update() } -> std::same_as<void>; // Update()がvoid型であることも要求
+};
+template <HaveUpdate Task>
+class TaskThread:
+	public IThread
+{
+public:
+	TaskThread()override {}
+	~TaskThread()override{}
+
+	void Init(int32_t threadNum)override;
+	void Finalize()override;
+private:
+	void LoopUpdate()override;
+
+	std::queue<Task> taskQueue_;
+public:
+	void pushTask(Task task)
+	{
+		std::unique_lock<std::mutex> lock(mutex_);
+		taskQueue_.emplace(task);
+		condition_.notify_one();
+	}
+};
+
+template<typename task>
+inline void TaskThread<task>::Init(int32_t threadNum)
+{
+	IThread::Init();
+}
+
+template<typename Task>
+inline void TaskThread<Task>::Finalize()
+{
+	IThread::Finalize();
+}
+
+template<typename Task>
+inline void TaskThread<Task>::LoopUpdate()
+{
+	while(true)
+	{
+		Task task;
+		{
+			std::unique_lock<std::mutex> lock(IThread::mutex_);
+			IThread::condition_.wait(lock,[this] { return !taskQueue_.empty() || IThread::stopThread_; });
+
+			if(IThread::stopThread_ && taskQueue_.empty())
+			{
+				return;
+			}
+			task = taskQueue_.front();
+			taskQueue_.pop();
+		}
+
+		{
+			std::lock_guard guard(queueMutex_);
+			task.Update();
+		}
+	}
+}
