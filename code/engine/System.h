dfeebdc030fa490e5f82d37761a0fa29460bb9d2
Amaimono <k023g0048@g.neec.ac.jp>
2024-10-21 14:23:01 +0900

    Flame Lock

--- code/engine/System.h
+++ code/engine/System.h
@@ -5,9 +5,11 @@
 #include <string>
 #include <vector>
 
-#include "input/Input.h"
-#include "winApp/WinApp.h"
-
+#include "deltaTime/DeltaTime.h"
+#include "directX12/buffer/light/DirectionalLight.h"
+#include "directX12/buffer/light/PointLight.h"
+#include "directX12/buffer/light/SpotLight.h"
+#include "directX12/buffer/Material.h"
 #include "directX12/dxCommand/DxCommand.h"
 #include "directX12/dxDevice/DxDevice.h"
 #include "directX12/dxFence/DxFence.h"
@@ -17,12 +19,9 @@
 #include "directX12/dxSwapChain/DxSwapChain.h"
 #include "directX12/PipelineStateObj.h"
 #include "directX12/shaderManager/ShaderManager.h"
-
-#include "directX12/buffer/light/DirectionalLight.h"
-#include "directX12/buffer/light/PointLight.h"
-#include "directX12/buffer/light/SpotLight.h"
-#include "directX12/buffer/Material.h"
+#include "input/Input.h"
 #include "model/Model.h"
+#include "winApp/WinApp.h"
 
 #include "Matrix4x4.h"
 #include "Vector2.h"
@@ -51,25 +50,30 @@ private:
 	const System& operator=(const System&) = delete;
 	void CreateTexturePSO();
 private:
+	// api
 	std::unique_ptr<WinApp> window_;
 	Input* input_;
 
+	// directX
 	std::unique_ptr<DxDevice> dxDevice_;
-
 	std::unique_ptr<DxCommand> dxCommand_;
 	std::unique_ptr<DxSwapChain> dxSwapChain_;
 	std::unique_ptr<DxFence> dxFence_;
-
 	std::unique_ptr<DxDsv> dxDsv_;
 
+	// PipelineState Object
 	std::array<PipelineStateObj*,kBlendNum> texturePso_;
 	std::array<std::string,kBlendNum> texturePsoKeys_;
 
+	// buffers
 	std::unique_ptr<MaterialManager> materialManager_;
-
 	std::unique_ptr<DirectionalLight> directionalLight_;
 	std::unique_ptr<PointLight> pointLight_;
 	std::unique_ptr<SpotLight> spotLight_;
+
+	// Time
+	std::unique_ptr<DeltaTime> deltaTime_;
+	float fps_ = 60.0f;
 public:
 	WinApp* getWinApp(){ return window_.get(); }
 
@@ -85,6 +89,8 @@ public:
 	PipelineStateObj* getTexturePso(BlendMode blend)const{ return texturePso_[static_cast<size_t>(blend)]; }
 
 	const std::array<std::string,kBlendNum>& getTexturePsoKeys()const{ return texturePsoKeys_; }
+
+	float getDeltaTime()const{return deltaTime_->getDeltaTime();}
 
 	DirectionalLight* getDirectionalLight()const{ return directionalLight_.get(); }
 	PointLight* getPointLight()const{ return pointLight_.get(); }
