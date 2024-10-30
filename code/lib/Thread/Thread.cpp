fdd9ead7b8f5a630f665a61e01107629c73f2fe6
Amaimono <k023g0048@g.neec.ac.jp>
2024-10-30 12:24:32 +0900

    Fix : thread

--- code/lib/Thread/Thread.cpp
+++ code/lib/Thread/Thread.cpp
@@ -1,28 +1,23 @@
 #include "Thread.h"
 
-void IThread::Init(int32_t threadNum)
-{
+void IThread::Init(int32_t threadNum){
 	stopThread_ = false;
 
 	threads_.resize(threadNum);
-	for(auto& thread : threads_)
-	{
+	for(auto& thread : threads_){
 		thread = std::thread(&IThread::Update,this);
 	}
 }
 
-void IThread::Finalize()
-{
+void IThread::Finalize(){
 	{
 		std::unique_lock<std::mutex> lock(mutex_);
 		stopThread_ = true;
 	}
 
 	condition_.notify_all();
-	for(std::thread& thread : threads_)
-	{
-		if(thread.joinable())
-		{
+	for(std::thread& thread : threads_){
+		if(thread.joinable()){
 			thread.join();  // スレッドの終了を待機
 		}
 	}
