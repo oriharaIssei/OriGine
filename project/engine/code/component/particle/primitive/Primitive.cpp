16a5ff2fe589d7e83ee4fd139bea2b28ad15b51f
OriharaIssei <k023g0048@g.neec.ac.jp>
2025-04-11 15:29:28 +0900

    途中

---
+++ project/engine/code/component/particle/primitive/Primitive.cpp
@@ -0,0 +1,76 @@
+#include "Primitive.h"
+
+void Circle::createMesh() {
+    mesh_->vertexes_.clear();
+    mesh_->indexes_.clear();
+    // 円の頂点を計算
+    for (uint32_t i = 0; i < vertexSize_; ++i) {
+        float angle    = static_cast<float>(i) * 2.0f * std::numbers::pi_v<float> / static_cast<float>(vertexSize_);
+        Vector3f point = {radius_ * std::cos(angle), 0.f, radius_ * std::sin(angle)};
+        mesh_->vertexes_.emplace_back(TextureVertexData(Vec4f(point, 1.f), Vec2f(), point.normalize()));
+    }
+    // 円のインデックスを計算
+    for (uint32_t i = 0; i < indexSize_; ++i) {
+        mesh_->indexes_.emplace_back(i);
+    }
+    mesh_->TransferData();
+}
+
+void Sphere::createMesh() {
+    mesh_->vertexes_.clear();
+    mesh_->indexes_.clear();
+
+    float divisionReal    = static_cast<float>(division_);
+    const float kLatEvery = std::numbers::pi_v<float> / divisionReal; //* 緯度
+    const float kLonEvery = 2.0f * std::numbers::pi_v<float> / divisionReal; //* 経度
+
+    auto calculatePoint = [&](float lat, float lon) -> Vector3f {
+        return {
+            radius_[X] * std::cos(lat) * std::cos(lon),
+            radius_[Y] * std::sin(lat),
+            radius_[Z] * std::cos(lat) * std::sin(lon)};
+    };
+
+    // 緯線（緯度方向の円）を描画
+    for (uint32_t latIndex = 1; latIndex < division_; ++latIndex) {
+        float lat = -std::numbers::pi_v<float> / 2.0f + kLatEvery * latIndex;
+        for (uint32_t lonIndex = 0; lonIndex < division_; ++lonIndex) {
+            float lonA = lonIndex * kLonEvery;
+            float lonB = (lonIndex + 1) % division_ * kLonEvery;
+
+            Vector3f pointA = calculatePoint(lat, lonA);
+            Vector3f pointB = calculatePoint(lat, lonB);
+
+            // 頂点バッファにデータを 格納
+            mesh_->vertexes_.emplace_back(TextureVertexData(Vec4f(pointA, 1.f), Vec2f(), pointA.normalize()));
+            mesh_->vertexes_.emplace_back(TextureVertexData(Vec4f(pointB, 1.f), Vec2f(), pointB.normalize()));
+            // インデックスバッファにデータを格納
+            mesh_->indexes_.emplace_back(uint32_t(mesh_->indexes_.size()));
+            mesh_->indexes_.emplace_back(uint32_t(mesh_->indexes_.size()));
+        }
+    }
+
+    // 経線（経度方向の円）を描画
+    for (uint32_t lonIndex = 0; lonIndex < division_; ++lonIndex) {
+        float lon = lonIndex * kLonEvery;
+        for (uint32_t latIndex = 0; latIndex < division_; ++latIndex) {
+            float latA = -std::numbers::pi_v<float> / 2.0f + kLatEvery * latIndex;
+            float latB = -std::numbers::pi_v<float> / 2.0f + kLatEvery * (latIndex + 1);
+
+            Vector3f pointA = calculatePoint(latA, lon);
+            Vector3f pointB = calculatePoint(latB, lon);
+
+            // 頂点バッファにデータを格納
+            mesh_->vertexes_.emplace_back(TextureVertexData(Vec4f(pointA, 1.f), Vec2f(), pointA.normalize()));
+            mesh_->vertexes_.emplace_back(TextureVertexData(Vec4f(pointB, 1.f), Vec2f(), pointB.normalize()));
+            // インデックスバッファにデータを格納
+            mesh_->indexes_.emplace_back(uint32_t(mesh_->indexes_.size()));
+            mesh_->indexes_.emplace_back(uint32_t(mesh_->indexes_.size()));
+        }
+    }
+
+    mesh_->TransferData();
+}
+
+void Ring::createMesh() {
+}
---
+++ project/engine/code/component/particle/primitive/Primitive.h
@@ -0,0 +1,257 @@
+#pragma once
+
+/// stl
+#include <memory>
+
+/// engine
+// dreictX12
+#include "directX12/Mesh.h"
+
+/// math
+#include <numbers>
+#include <Vector2.h>
+#include <Vector3.h>
+#include <Vector4.h>
+
+enum class PrimitiveType {
+    Plane, // 面
+    Circle, // 円
+    Ring, // 環(真ん中が空洞)
+    Sphere, // 球
+    Torus, // トーラス
+    Cylinder, // 円柱
+    Cone, // 円錐
+    Triangel, // 三角形
+    Box // 立方体
+};
+
+/// <summary>
+/// 形状の基底クラス
+/// </summary>
+class IPrimitive {
+public:
+    IPrimitive(PrimitiveType _type) : type_(_type) {}
+    virtual ~IPrimitive() {
+        mesh_.reset();
+    }
+
+    virtual void Initialize() {
+        if (mesh_ == nullptr) {
+            createMesh();
+        }
+    }
+    virtual void createMesh() = 0;
+
+protected:
+    std::shared_ptr<TextureMesh> mesh_ = nullptr;
+
+private:
+    PrimitiveType type_;
+
+public: // accessor
+    PrimitiveType getType() const { return type_; }
+
+    const std::shared_ptr<TextureMesh>& getMesh() const {
+        return mesh_;
+    }
+};
+
+class Plane
+    : public IPrimitive {
+public:
+    Plane() : IPrimitive(PrimitiveType::Plane) {}
+    ~Plane() override {
+        mesh_.reset();
+    }
+    void Initialize() override {
+        // mesh Init
+        mesh_ = std::make_shared<TextureMesh>();
+        mesh_->setName("Plane");
+        mesh_->Initialize(4, 6);
+
+        // create mesh
+        createMesh();
+    }
+
+    void createMesh() override {
+        mesh_->setVertexData({{Vec4f(-size_[X], 0.0f, -size_[Y], 1.0f), Vec2f(0.0f, 0.0f), normal_},
+            {Vec4f(size_[X], 0.0f, -size_[Y], 1.0f), Vec2f(uv_[X], 0.0f), normal_},
+            {Vec4f(-size_[X], 0.0f, size_[Y], 1.0f), Vec2f(0.0f, uv_[Y]), normal_},
+            {Vec4f(size_[X], 0.0f, size_[Y], 1.0f), Vec2f(uv_[X], uv_[Y]), normal_}});
+        mesh_->setIndexData({2, 3, 1,
+            2, 1, 0});
+        mesh_->TransferData();
+    }
+
+private:
+    Vec2f size_   = {1.0f, 1.0f};
+    Vec2f uv_     = {1.0f, 1.0f};
+    Vec3f normal_ = {0.0f, 0.0f, 1.0f};
+
+public: // accessor
+    const Vec2f& getSize() const {
+        return size_;
+    }
+    void setSize(const Vec2f& _size) {
+        size_ = _size;
+    }
+    const Vec2f& getUV() const {
+        return uv_;
+    }
+    void setUV(const Vec2f& _uv) {
+        uv_ = _uv;
+    }
+    const Vec3f& getNormal() const {
+        return normal_;
+    }
+    void setNormal(const Vec3f& _normal) {
+        normal_ = _normal;
+    }
+};
+
+class Circle
+    : public IPrimitive {
+public:
+    Circle() : IPrimitive(PrimitiveType::Circle) {}
+    ~Circle() override {
+        mesh_.reset();
+    }
+    void Initialize() override {
+        // mesh Init
+        mesh_ = std::make_shared<TextureMesh>();
+        mesh_->setName("Circle");
+        mesh_->Initialize(vertexSize_, indexSize_);
+        // create mesh
+        createMesh();
+    }
+    void createMesh() override;
+
+private:
+    uint32_t vertexSize_ = 32;
+    uint32_t indexSize_  = 32;
+
+    float radius_ = 1.f;
+
+public: // accessor
+    float getRadius() const {
+        return radius_;
+    }
+    void setRadius(float _radius) {
+        radius_ = _radius;
+    }
+
+    uint32_t getVertexSize() const {
+        return vertexSize_;
+    }
+    void setVertexSize(uint32_t _vertexSize_) {
+        vertexSize_ = _vertexSize_;
+    }
+};
+
+class Ring
+    : public IPrimitive {
+public:
+    Ring() : IPrimitive(PrimitiveType::Ring) {}
+    ~Ring() override {
+        mesh_.reset();
+    }
+    void Initialize() override {
+        vertexSize_ = 4 * division_ * division_;
+        indexSize_  = 4 * division_ * division_;
+        // mesh Init
+        mesh_ = std::make_shared<TextureMesh>();
+        mesh_->setName("Ring");
+        mesh_->Initialize(vertexSize_, indexSize_);
+        // create mesh
+        createMesh();
+    }
+    void createMesh() override;
+
+private:
+    uint32_t division_   = 16;
+    uint32_t vertexSize_ = 0;
+    uint32_t indexSize_  = 0;
+
+    float radius_      = 1.f;
+    float innerRadius_ = 0.5f;
+    float uv_          = 1.f;
+    float uvInner_     = 0.5f;
+};
+
+class Sphere
+    : public IPrimitive {
+public:
+    Sphere() : IPrimitive(PrimitiveType::Sphere) {}
+    ~Sphere() override {
+        mesh_.reset();
+    }
+    void Initialize() override {
+        vertexSize_ = 4 * division_ * division_;
+        indexSize_  = 4 * division_ * division_;
+        // mesh Init
+        mesh_ = std::make_shared<TextureMesh>();
+        mesh_->setName("Sphere");
+        mesh_->Initialize(vertexSize_, indexSize_);
+        // create mesh
+        createMesh();
+    }
+    void createMesh() override;
+
+private:
+    uint32_t division_   = 16;
+    uint32_t vertexSize_ = 0;
+    uint32_t indexSize_  = 0;
+
+    Vec3f radius_ = {1.f, 1.f, 1.f};
+    Vec3f uv_     = {1.f, 1.f, 1.f};
+
+public: // accessor
+    const Vec3f& getRadius() const {
+        return radius_;
+    }
+    void setRadius(const Vec3f& _radius) {
+        radius_ = _radius;
+    }
+
+    const Vec3f& getUV() const {
+        return uv_;
+    }
+    void setUV(const Vec3f& _uv) {
+        uv_ = _uv;
+    }
+
+    uint32_t getDivision() const {
+        return division_;
+    }
+    void setDivision(uint32_t _division) {
+        division_ = _division;
+    }
+};
+
+class Torus
+    : public IPrimitive {
+public:
+    Torus() : IPrimitive(PrimitiveType::Torus) {}
+    ~Torus() override {
+        mesh_.reset();
+    }
+    void Initialize() override {
+        vertexSize_ = 4 * division_ * division_;
+        indexSize_  = 4 * division_ * division_;
+        // mesh Init
+        mesh_ = std::make_shared<TextureMesh>();
+        mesh_->setName("Torus");
+        mesh_->Initialize(vertexSize_, indexSize_);
+        // create mesh
+        createMesh();
+    }
+    void createMesh() override;
+
+private:
+    uint32_t division_ = 16;
+    uint32_t vertexSize_;
+    uint32_t indexSize_;
+
+    float radius_ = 1.f;
+    float uv_     = 1.f;
+};
