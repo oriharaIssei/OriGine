a40697acd568d2fcfd8ba6192b4bef18b3363e86
OriharaIssei <k023g0048@g.neec.ac.jp>
2025-05-28 16:22:57 +0900

    searchFileDialogの返り値にファイルフォーマットがついているかいないかを選べるように

--- project/lib/myFileSystem/MyFileSystem.h
+++ project/lib/myFileSystem/MyFileSystem.h
@@ -20,37 +20,12 @@ public:
     static bool createFolder(const std::string& directory);
 
     static void selectFolderDialog(const std::string& _defaultDirectory, std::string& _outPath);
-    static bool selectFileDialog(const std::string& defaultDirectory, std::string& fileDirectory, std::string& filename, const std::vector<std::string>& extensions);
+    static bool selectFileDialog(
+        const std::string& defaultDirectory, std::string& fileDirectory, std::string& filename, const std::vector<std::string>& extensions, bool withoutExtensionOutput = false);
 
     static bool removeEmptyFolder(const std::string& directory);
     static std::uintmax_t deleteFolder(const std::string& path);
     static std::uintmax_t deleteFile(const std::string& filePath);
-
-#ifdef _DEBUG
-    /// <summary>
-    /// GUIのファイル選択ダイアログを表示
-    /// </summary>
-    /// <param name="_defaultDirectory">初期表示するフォルダ</param>
-    /// <param name="_fileDirectory">選択したフォルダ(_defaultDirectoryから見た directory)</param>
-    /// <param name="_filename">選択したファイル名</param>
-    /// <param name="_extensions">拡張子のリスト</param>
-    /// <returns>選択したかどうか</returns>
-    /// <remarks>拡張子は「.png」などの形式で指定</remarks>
-    // static bool selectFileGuiDialog(
-    //     const std::string& _defaultDirectory,
-    //     std::string& _fileDirectory,
-    //     std::string& _filename,
-    //     const std::vector<std::string>& _extensions);
-
-    /// <summary>
-    /// GUIのフォルダ選択ダイアログを表示
-    /// </summary>
-    /// <param name="_defaultDirectory">初期表示するフォルダ</param>
-    /// <param name="_outPath">選択したフォルダ(_defaultDirectoryから見た directory)</param>
-    /// <returns>選択したかどうか</returns>
-    // static bool selectFolderGuiDialog(const std::string& _defaultDirectory, std::string& _outPath);
-
-#endif // _DEBUG
 };
 
 using myfs = MyFileSystem;
