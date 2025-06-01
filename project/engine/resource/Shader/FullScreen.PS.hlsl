d0063df238cb62451bf3918cd86f7e1549799983
OriharaIssei <k023g0048@g.neec.ac.jp>
2025-06-02 01:29:11 +0900

    描画 修正

--- project/engine/resource/Shader/FullScreen.PS.hlsl
+++ project/engine/resource/Shader/FullScreen.PS.hlsl
@@ -3,8 +3,10 @@
 Texture2D<float4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
-PixelShaderOutput main(VertexShaderOutput input) {
+PixelShaderOutput main(VertexShaderOutput input)
+{
     PixelShaderOutput output;
-    output.color = gTexture.Sample(gSampler,input.texCoords);
+    output.color = gTexture.Sample(gSampler, input.texCoords);
+    output.color.a = 1.0f; // Fixed alpha value
     return output;
 }
