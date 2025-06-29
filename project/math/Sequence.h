47dec176efcd716dbd9531862e927a968966d9cd
OriharaIssei <k023g0048@g.neec.ac.jp>
2025-06-29 23:07:19 +0900

    CommonSequenceType 追加

--- project/math/Sequence.h
+++ project/math/Sequence.h
@@ -8,6 +8,23 @@
 // 数列
 /// ==========================================
 
+enum class CommonSequenceType {
+    Arithmetic, // 等差数列
+    Geometric // 等比数列
+};
+namespace std {
+std::string to_string(CommonSequenceType _type) {
+    switch (_type) {
+    case CommonSequenceType::Arithmetic:
+        return "Arithmetic";
+    case CommonSequenceType::Geometric:
+        return "Geometric";
+    default:
+        return "Unknown";
+    }
+}
+}
+
 /// <summary>
 /// 等差数列
 /// </summary>
@@ -31,6 +48,18 @@ T ArithmeticSequence(T _base, T _commonDifference, int _index) {
 template <typename T>
 T GeometricSequence(T _base, T _commonRatio, int _index) {
     return _base * pow(_commonRatio, T(_index - 1));
+}
+
+template <typename T>
+T CommonSequence(CommonSequenceType _type, T _base, T _commonValue, int _index) {
+    switch (_type) {
+    case CommonSequenceType::Arithmetic:
+        return ArithmeticSequence(_base, _commonValue, _index);
+    case CommonSequenceType::Geometric:
+        return GeometricSequence(_base, _commonValue, _index);
+    default:
+        throw std::invalid_argument("Unsupported sequence type");
+    }
 }
 
 /// <summary>
