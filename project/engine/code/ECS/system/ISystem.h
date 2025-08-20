d98cfe45ea7cbdd50eeeafa364721efe00bd9c93
OriharaIssei <k023g0048@g.neec.ac.jp>
2025-08-20 11:07:26 +0900

    CreateEntity() 追加

--- project/engine/code/ECS/system/ISystem.h
+++ project/engine/code/ECS/system/ISystem.h
@@ -67,7 +67,8 @@ protected:
     /// ==========================================
 
     GameEntity* getEntity(int32_t _entityID) ;
-    GameEntity* getUniqueEntity(const std::string& _dataTypeName) ;
+    GameEntity* getUniqueEntity(const std::string& _dataTypeName);
+    int32_t CreateEntity(const std::string& _dataTypeName,bool _isUnique = false);
 
     IComponentArray* getComponentArray(const std::string& _typeName);
     template <IsComponent ComponentType>
